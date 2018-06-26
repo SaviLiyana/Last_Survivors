06ebb9c7073106edc9329275e83268f826993690
Namalka Samarakoon <39297985+NamalkaSamarakoon@users.noreply.github.com>
2018-06-27 04:11:26 +0530

    Update brokertransactions.php
    new

--- application/views/client/broker/brokertransactions.php
+++ application/views/client/broker/brokertransactions.php
@@ -1,15 +1,12 @@
 
 <div class="container "><br/>
-
 <br/>
-
    <div class="col-md-3">
      <h6>Date</h6>
       <input type="date" id="Date" class="form-control column_filter_date" data-column="0" >
    </div>
   <div class="table-responsive">
     <table id="DataTable" class="table table-hover" style="width: 100%; float: left;">
-
       <thead>
         <tr>
           <th>Date:Time</th>
@@ -21,9 +18,7 @@
           <th>Total</th>
         </tr>
       </thead>
-
       <tbody>
-
         <?php foreach ($transactions as $transaction) : ?>
         <tr>
           <td><?php echo $transaction["Create_Date"] ?></td>
@@ -35,13 +30,9 @@
           <td><?php echo ($transaction["amount"])*($transaction["qty"]) ?></td>
         </tr>
         <?php endforeach; ?>
-
       </tbody>
     </table>
   </div>
-
-
-
 </div>
 <script>
 setTimeout(function() {
