fd4a985a7770a1b1ed6c74b73333e680b75130b5
Dulaj Nikeshala <dulajnikeshala@gmail.com>
2018-06-27 00:10:50 +0530

    Add files via upload
    modified_controllers

--- application/controllers/Shares.php
+++ application/controllers/Shares.php
@@ -1,33 +1,17 @@
 <?php
   class Shares extends CI_Controller{
-    public function view($share_id = NULL){
+    public function view(){
       if($this->session->userdata('admin','logged_in'))
       {
-        if($share_id == NULL){
-            $data['categories']=$this->Shares_model->get_categories();
-            $data['sector_details'] = $this->Shares_model->get_companies();
-            $page['title'] = "Shares";
-            $page['adminuser'] = $this->session->userdata('admin');
-            $this->load->view('admin/template/header', $page);
-            $this->load->view('admin/share/index', $data);
-            $this->load->view('admin/template/footer');
-        }
-        else{
-          $data['share'] = $this->Client_broker_share_selected_model->show_one_share($share_id);
-          $data['share_value'] = $this->Client_broker_share_selected_model->share_values($share_id);
-          $data['buy_orders'] = $this->Client_broker_share_selected_model->buy_orders($share_id);
-          $data['sell_orders'] = $this->Client_broker_share_selected_model->sell_orders($share_id);
-
-          $page['adminuser'] = $this->session->userdata('admin');
+          $data['categories']=$this->Shares_model->get_categories();
+          $data['sector_details'] = $this->Shares_model->get_companies();
           $page['title'] = "Shares";
-
+          $page['adminuser'] = $this->session->userdata('admin');
           $this->load->view('admin/template/header', $page);
-          $this->load->view('admin/share/view', $data);
+          $this->load->view('admin/share/index', $data);
           $this->load->view('admin/template/footer');
-        }
       }
       else {
-        $this->session->set_flashdata('logged_out','Please Log In...!');
         redirect('admin');
       }
     }
