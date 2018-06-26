25256d7f4dc570bfe9178957a0f7fe48af6c6249
Ruvinilakshini <39304166+lakruvini@users.noreply.github.com>
2018-06-27 04:09:08 +0530

    Add files via upload
    adv

---
+++ application/models/Client_broker_advises_model.php
@@ -0,0 +1,22 @@
+<?php
+class Client_broker_advises_model extends CI_Model{
+  public function __construct(){
+    $this->load->database();
+  }
+
+  public function show_gain_data(){
+    $this->db->where('(((c_current_share_value-c_starting_share_value)/c_starting_share_value)*100) >=' , 50 );
+    $quary=$this->db->get('company');
+    return $quary->result_array();
+
+  }
+
+  public function show_loss_data(){
+    $this->db->where('(((c_current_share_value-c_starting_share_value)/c_starting_share_value)*100) <=' , 20 );
+    $quary=$this->db->get('company');
+    return $quary->result_array();
+    
+  }
+  
+}
+?>
