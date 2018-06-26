d54673a4bfd1720b74cb07ced8ce6f9c7366575b
Ruvinilakshini <39304166+lakruvini@users.noreply.github.com>
2018-06-27 04:15:06 +0530

    Update Clientdevelopers.php
    new

--- application/controllers/Clientdevelopers.php
+++ application/controllers/Clientdevelopers.php
@@ -1,11 +1,9 @@
 <?php
   class Clientdevelopers extends CI_Controller{
     public function view(){
-
       $page['userdata_broker'] = $this->session->userdata('broker');
       $page['title'] = "DevelopingTeam";
       $page['turn'] = $this->Client_broker_home_model->get_current_turn();
-
       $this->load->view('client/template/header', $page);
       $this->load->view('client/developers/team');
       $this->load->view('client/template/footer');
